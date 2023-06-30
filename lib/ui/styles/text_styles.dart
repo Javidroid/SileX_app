7031b1cf5d8c3c6138ba6391f2d471aae750e452
Javier Florido <javierfloridocartolano@gmail.com>
2023-07-01 01:24:37 +0200

    feat: added insets and declared text styles

---
+++ lib/ui/styles/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Fonts {
+  // TODO: import fonts
+  static const String futuraLight = 'Futura-Light';
+  static const String futuraMedium = 'Futura-Medium';
+  static const String futuraBold = 'Futura-Bold';
+  static const String futuraExtraBold = 'Futura-ExtraBold';
+}
+
+class TextStyles {
+  // TODO: define & improve default text styles
+
+  static const TextStyle customerServiceTitle = TextStyle();
+}
