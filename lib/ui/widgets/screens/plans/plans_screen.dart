57cb1a7082980c5cadf2b20f694f9c325b52f7c8
Javier Florido <javierfloridocartolano@gmail.com>
2023-07-30 23:26:35 +0200

    changed plan text to widget

--- lib/ui/widgets/screens/plans/plans_screen.dart
+++ lib/ui/widgets/screens/plans/plans_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tfg_v2/di/dependency_injection.dart';
 import 'package:tfg_v2/ui/viewmodel/plans/plans_viewmodel.dart';
+import 'package:tfg_v2/ui/widgets/components/items/plan_preview_item.dart';
 import 'package:tfg_v2/ui/widgets/screens/root_screen.dart';
 
 class PlansScreen extends RootScreen<PlansViewState> {
@@ -20,7 +21,8 @@ class PlansScreen extends RootScreen<PlansViewState> {
         Success _ => Center(
             child: Column(
               mainAxisSize: MainAxisSize.min,
-              children: state.planList.map((e) => Text(e.toString())).toList(),
+              children:
+                  state.planList.map((e) => PlanPreviewItem(plan: e)).toList(),
             ),
           ),
         Error _ => Text(state.error.toString()), // todo handle errors
